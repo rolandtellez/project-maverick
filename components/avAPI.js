87df059971a05198068226b0c6e5756072e092dc
jeffery.raimundo@medaire.com <jeff_pepper23@yahoo.com>
2019-07-05 20:35:19 -0700

    routes & API coded -- not firing yet

---
+++ components/avAPI.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+require("../server")
+const axios = require("axios");
+const apiKey = process.env.API_KEY;
+console.log(apiKey);
+console.log(process.env.API_KEY);
+
+function searchFlights(flightData, cb) {
+
+    //let flightParameters = {};
+
+    axios.get("http://aviation-edge.com/v2/public/flights?key=" + apiKey + "&flightIata=aa258").then(function(response) {
+        console.log(response.data);
+        //console.log(response.data[0].geography.latitude);
+
+        cb(response.data);
+
+    }).catch(e => {
+        console.log(e);
+    });
+};
+
+searchFlights();
+module.exports = searchFlights;
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+//console.log(process.env.API_KEY);
+//const db = require("./models");
 
 const app = express();
+const PORT = process.env.PORT || 7500;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/api/team", (req,res) => {
     let team = [
@@ -11,8 +18,10 @@ app.get("/api/team", (req,res) => {
         {id: 4, firstName: "Roland", lastName: "Tellez"},
     ];
     res.json(team);
-})
+});
 
-const PORT = process.env.PORT || 7500;
+
 
 app.listen(PORT, () => console.log("Successful Connection:  Visit http://localhost/ in you browser port: "  + PORT));
+
+module.exports = app;
