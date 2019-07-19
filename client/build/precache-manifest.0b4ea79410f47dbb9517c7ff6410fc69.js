60eb2113d66b8d6608e768c34696a5dcc786b808
jeffery.raimundo@medaire.com <jeff_pepper23@yahoo.com>
2019-07-18 20:36:35 -0700

    Build file Blues

---
+++ client/build/precache-manifest.0b4ea79410f47dbb9517c7ff6410fc69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0933fab8fb8a55a03664dad8145a3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27d46f88efd1334f18ef",
+    "url": "/static/css/main.bf1d1017.chunk.css"
+  },
+  {
+    "revision": "7dba58c3381eaa341270",
+    "url": "/static/js/2.018541f6.chunk.js"
+  },
+  {
+    "revision": "27d46f88efd1334f18ef",
+    "url": "/static/js/main.ad62ee67.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04b0f886e5375ab839c24fe712c6c6cf",
+    "url": "/static/media/combined-bg.04b0f886.png"
+  }
+]);
